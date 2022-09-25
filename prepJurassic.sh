e28bae7df89c67b0e9643479988a395cf5626b6b
bob.chesebrough <bob.chesebrough@intel.com>
2022-09-25 11:45:14 -0700

    add prepJurassic.sh, tweaking 7 thru 10

---
+++ prepJurassic.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+cp /data/oneapi_workshop/big_datasets/jurassic/*.zip .
+unzip data.zip
+unzip model.zip
+source /glob/development-tools/versions/oneapi/2022.2/oneapi/setvars.sh --force
+conda activate base
+pip install -r requirements.txt
+echo "create new environment"
+conda create --clone pytorch --name openvinopytorch2
+echo "activate environment"
+conda activate openvinopytorch2
+echo "install openvino-dev"
+~/.conda/envs/openvinopytorch/bin/pip install --user openvino-dev[pytorch]
+echo "register new kernel in Jupyter"
+python -m ipykernel install --user --name openvinopytorch2
+echo "install OpenVINO lan requirements"
+pip install -r requirements_openvino.txt
+conda activate openvinopytorch2
