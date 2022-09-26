be493180f519ca584a4ad5980f77de6a1d89278f
bob.chesebrough <bob.chesebrough@intel.com>
2022-09-25 20:42:06 -0700

    add updated prepJurassicLite.sh

---
+++ prepJurassicLite.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "Gathering data"
+cp /data/oneapi_workshop/big_datasets/jurassic/*.zip .
+unzip -qq data.zip
+echo "Gathering Models"
+mv models.zip models0.zip
+wget https://www.dropbox.com/s/tj8j6infag4sfcf/models.zip
+unzip -qq models.zip
+echo "Setting conda environment"
+source /glob/development-tools/versions/oneapi/2022.2/oneapi/setvars.sh --force
+conda activate base
+pip install -r requirements.txt
+# echo "creating new environment"
+# conda create --clone pytorch --name openvinopytorch2
+# echo "Activating environment"
+# conda activate openvinopytorch2
+# echo "Instalingl openvino-dev"
+# ~/.conda/envs/openvinopytorch/bin/pip install --user openvino-dev[pytorch]
+# echo "Registering new kernel in Jupyter"
+# python -m ipykernel install --user --name openvinopytorch2
+# echo "Installing OpenVINO lan requirements"
+# pip install -r requirements_openvino.txt
+conda activate base
