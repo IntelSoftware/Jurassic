4170038bd12557a0fea746da952d8e9a0990b812
Bob Chesebrough <bob.chesebrough@intel.com>
2024-02-02 20:56:43 -0800

    add png in DinosaurNationalMonument to correct bug in 05

--- prepJurassic.sh
+++ prepJurassic.sh
@@ -3,6 +3,7 @@
 echo "Gathering data"
 cp -R ~/Big_Data/jurassic/ data/
 mv data/jurassic/data/DinosaurNationalMonument 
+cp assets/m_4010938_ne_12_060_20211004_bc_cropped2.png data/DinosaurNationalMonument/
 echo "Gathering Models"
 mkdir models
 echo "Setting conda environment"
